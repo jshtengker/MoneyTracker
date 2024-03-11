9d0dfcfa57e70717ce4bd399052fcb5986c14825
jshtengker <leonardotengker@gmail.com>
2024-03-11 18:15:57 +0800

    1. install react navigation 2. router untuk stack navigator 3. refactor pages

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Sign In</Text>
+    </View>
+  );
+};
+
+export default SignIn;
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+
+  },
+})
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+import SignIn from './SignIn';
+import SplashScreen from './SplashScreen';
+
+export {SignIn, SplashScreen};
